846b5dd0192e0f184c941f5f914ad379e82fec93
doodspav <doodspav@gmail.com>
2026-06-28 00:20:18 +0100

    Set up the patomic package properly

--- patomic/src/lib.rs
+++ patomic/src/lib.rs
@@ -4,11 +4,14 @@ pub fn add(left: u64, right: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
+    use patomic_sys::sub;
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
+        let y = sub(6, 2);
         assert_eq!(result, 4);
+        assert_eq!(y, result);
     }
 }
