2e3b31a2195068921e96777c6afc5d89b06c8188
doodspav <doodspav@gmail.com>
2026-06-28 02:18:49 +0100

    Update comments in build.rs

--- patomic-sys/build.rs
+++ patomic-sys/build.rs
@@ -1,15 +1,15 @@
 fn main() {
-    // build vendored patomic library statically
+    // build the vendored patomic library statically
     let install_prefix = cmake::Config::new("vendor/patomic")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("CMAKE_INSTALL_LIBDIR", "lib")
         .build();
 
-    // add CMake's LIB_DIR to search path
+    // add CMake's LIB_DIR to the search path
     let lib_dir = install_prefix.join("lib");
     println!("cargo:rustc-link-search=native={}", lib_dir.display());
 
-    // set "patomic" as expected library name
+    // set "patomic" as the expected library name
     println!("cargo:rustc-link-lib=static=patomic");
 
     // rebuild when needed
