c7d510b8a28b0778e83ca8b4b2cd5ed3c0e1e89b
doodspav <doodspav@gmail.com>
2026-06-28 04:43:53 +0100

    Add tests for memory order macros

--- patomic-sys/src/memory_order.rs
+++ patomic-sys/src/memory_order.rs
@@ -83,3 +83,163 @@ unsafe extern "C" {
         succ: c_int,
     ) -> c_int;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INVALID_ORDERS: [c_int; 5] = [-1, -10, 10, c_int::MIN, c_int::MAX];
+
+    const VALID_ORDERS: [patomic_memory_order_t; 6] = [
+        patomic_RELAXED,
+        patomic_CONSUME,
+        patomic_ACQUIRE,
+        patomic_RELEASE,
+        patomic_ACQ_REL,
+        patomic_SEQ_CST,
+    ];
+
+    const STORE_ORDERS: [patomic_memory_order_t; 3] = [
+        patomic_RELAXED,
+        patomic_RELEASE,
+        patomic_SEQ_CST,
+    ];
+
+    const LOAD_ORDERS: [patomic_memory_order_t; 4] = [
+        patomic_RELAXED,
+        patomic_CONSUME,
+        patomic_ACQUIRE,
+        patomic_SEQ_CST,
+    ];
+
+    #[test]
+    fn is_valid_order_allows_all_valid_orders() {
+        for order in VALID_ORDERS {
+            assert!(PATOMIC_IS_VALID_ORDER(order));
+        }
+    }
+
+    #[test]
+    fn is_valid_order_rejects_invalid_orders() {
+        for order in INVALID_ORDERS {
+            assert!(!PATOMIC_IS_VALID_ORDER(order));
+        }
+    }
+
+    #[test]
+    fn is_valid_store_order_allows_all_valid_store_orders() {
+        for order in STORE_ORDERS {
+            assert!(PATOMIC_IS_VALID_STORE_ORDER(order));
+        }
+    }
+
+    #[test]
+    fn is_valid_store_order_rejects_invalid_orders() {
+        for order in INVALID_ORDERS {
+            assert!(!PATOMIC_IS_VALID_STORE_ORDER(order));
+        }
+    }
+
+    #[test]
+    fn is_valid_store_order_rejects_non_store_orders() {
+        for order in VALID_ORDERS {
+            if !STORE_ORDERS.contains(&order) {
+                assert!(!PATOMIC_IS_VALID_STORE_ORDER(order));
+            }
+        }
+    }
+
+    #[test]
+    fn is_valid_load_order_allows_all_valid_load_orders() {
+        for order in LOAD_ORDERS {
+            assert!(PATOMIC_IS_VALID_LOAD_ORDER(order));
+        }
+    }
+
+    #[test]
+    fn is_valid_load_order_rejects_invalid_load_orders() {
+        for order in INVALID_ORDERS {
+            assert!(!PATOMIC_IS_VALID_LOAD_ORDER(order));
+        }
+    }
+
+    #[test]
+    fn is_valid_load_order_rejects_non_load_orders() {
+        for order in VALID_ORDERS {
+            if !LOAD_ORDERS.contains(&order) {
+                assert!(!PATOMIC_IS_VALID_LOAD_ORDER(order));
+            }
+        }
+    }
+
+    #[test]
+    fn is_valid_fail_order_allows_all_valid_pairs() {
+        for succ in VALID_ORDERS {
+            for fail in VALID_ORDERS {
+                if fail > succ || !LOAD_ORDERS.contains(&fail) {
+                    continue;
+                }
+                assert!(PATOMIC_IS_VALID_FAIL_ORDER(succ, fail));
+            }
+        }
+    }
+
+    #[test]
+    fn is_valid_fail_order_rejects_succ_lt_fail() {
+        for succ in VALID_ORDERS {
+            for fail in VALID_ORDERS {
+                if succ < fail {
+                    assert!(!PATOMIC_IS_VALID_FAIL_ORDER(succ, fail));
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn is_valid_fail_order_rejects_invalid_succ_order() {
+        for succ in INVALID_ORDERS {
+            for fail in VALID_ORDERS {
+                assert!(!PATOMIC_IS_VALID_FAIL_ORDER(succ, fail));
+            }
+        }
+    }
+
+    #[test]
+    fn is_valid_fail_order_rejects_invalid_fail_order() {
+        for fail in INVALID_ORDERS {
+            for succ in VALID_ORDERS {
+                assert!(!PATOMIC_IS_VALID_FAIL_ORDER(succ, fail));
+            }
+        }
+    }
+
+    #[test]
+    fn is_valid_fail_order_rejects_non_load_fail_order() {
+        for fail in VALID_ORDERS {
+            if !LOAD_ORDERS.contains(&fail) {
+                for succ in VALID_ORDERS {
+                    assert!(!PATOMIC_IS_VALID_FAIL_ORDER(succ, fail));
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn cmpxchg_fail_order_converts_valid_succ_order() {
+        for order in VALID_ORDERS {
+            let fail_order = PATOMIC_CMPXCHG_FAIL_ORDER(order);
+            if LOAD_ORDERS.contains(&order) {
+                assert_eq!(order, fail_order);
+            } else {
+                assert_eq!(patomic_ACQUIRE, fail_order);
+            }
+        }
+    }
+
+    #[test]
+    fn cmpxchg_fail_order_returns_invalid_succ_order() {
+        for order in INVALID_ORDERS {
+            assert_eq!(order, PATOMIC_CMPXCHG_FAIL_ORDER(order));
+        }
+    }
+}
