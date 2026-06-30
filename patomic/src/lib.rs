13d2a00c54f2029b731a8264979757aa18b19c96
doodspav <doodspav@gmail.com>
2026-06-30 15:12:54 +0100

    Add version and test for it

--- patomic/src/lib.rs
+++ patomic/src/lib.rs
@@ -6,12 +6,15 @@ mod ops;
 
 use align::*;
 
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use patomic_sys as sys;
 
     #[test]
-    fn it_works() {
-        assert!(true);
+    fn version_matches_sys_version() {
+        assert_eq!(VERSION, sys::PATOMIC_VERSION_STRING);
     }
 }
