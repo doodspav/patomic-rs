0cd5d9c17f17a9012e0363cdbcaefe9206696c3a
doodspav <doodspav@gmail.com>
2026-06-30 15:43:10 +0100

    Add more error cases

--- patomic/src/error.rs
+++ patomic/src/error.rs
@@ -3,8 +3,11 @@
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Error {
-    InvalidAlignment,
     InvalidSize,
+    InvalidAlignment,
+    InvalidMemoryOrder,
+    InvalidFailOrder,
+    UnsupportedOperation,
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
@@ -12,8 +15,11 @@ pub type Result<T> = core::result::Result<T, Error>;
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
-            Self::InvalidAlignment => write!(f, "invalid alignment"),
             Self::InvalidSize => write!(f, "invalid size"),
+            Self::InvalidAlignment => write!(f, "invalid alignment"),
+            Self::InvalidMemoryOrder => write!(f, "invalid memory order"),
+            Self::InvalidFailOrder => write!(f, "invalid fail order"),
+            Self::UnsupportedOperation => write!(f, "unsupported operation"),
         }
     }
 }
