a8c7745dfdc826baa1940a8f768b2769a01d4f3c
doodspav <doodspav@gmail.com>
2026-07-01 09:33:37 +0100

    Add SharedFlag(Ref) types without constructors

--- patomic/src/shared.rs
+++ patomic/src/shared.rs
@@ -4,6 +4,7 @@
 use core::cell::UnsafeCell;
 
 #[derive(Clone, Copy)]
+#[repr(transparent)]
 pub struct SharedBytesRef<'a> {
     bytes: &'a [UnsafeCell<u8>],
 }
@@ -30,5 +31,43 @@ impl<'a> SharedBytesRef<'a> {
 
     pub fn as_mut_ptr(&self) -> *mut u8 {
         self.bytes.as_ptr() as *mut u8
+    }
+}
+
+#[derive(Default)]
+#[repr(transparent)]
+pub struct SharedFlag {
+    flag: UnsafeCell<u8>,
+}
+
+unsafe impl Send for SharedFlag {}
+unsafe impl Sync for SharedFlag {}
+
+impl SharedFlag {
+    pub fn as_ptr(&self) -> *const u8 {
+        self.flag.get()
+    }
+
+    pub fn as_mut_ptr(&self) -> *mut u8 {
+        self.flag.get()
+    }
+}
+
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct SharedFlagRef<'a> {
+    flag: &'a SharedFlag,
+}
+
+unsafe impl Send for SharedFlagRef<'_> {}
+unsafe impl Sync for SharedFlagRef<'_> {}
+
+impl<'a> SharedFlagRef<'a> {
+    pub fn as_ptr(&self) -> *const u8 {
+        self.flag.as_ptr()
+    }
+
+    pub fn as_mut_ptr(&self) -> *mut u8 {
+        self.flag.as_mut_ptr()
     }
 }
