7585c0821d384697e43df1b9961264da710c949a
doodspav <doodspav@gmail.com>
2026-07-02 22:07:12 +0100

    Add unchecked transaction ldst ops

--- patomic/src/ops/unchecked_transaction.rs
+++ patomic/src/ops/unchecked_transaction.rs
@@ -1,15 +1,46 @@
 // Copyright (c) doodspav.
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use core::ffi::{c_int, c_void};
+use core::mem::MaybeUninit;
+
 use patomic_sys::*;
 
-use crate::SharedFlagRef;
+use crate::{SharedBytesRef, SharedFlagRef, transaction::*};
 
 pub unsafe trait FfiOpsTransaction {
     fn ffi_ops(&self) -> patomic_ops_transaction_t;
 }
 
 pub trait UncheckedTransactionOps: FfiOpsTransaction {
+    unsafe fn unchecked_store_transaction(
+        &self, obj: SharedBytesRef, desired: &[u8], config: TransactionConfig
+    ) -> TransactionOutcome {
+        let fp_store = self.ffi_ops().fp_store.unwrap_unchecked();
+        let mut outcome = MaybeUninit::uninit();
+        fp_store(
+            obj.as_mut_ptr() as *mut c_void,
+            desired.as_ptr() as *const c_void,
+            config.into(),
+            outcome.as_mut_ptr(),
+        );
+        outcome.assume_init().into()
+    }
+
+    unsafe fn unchecked_load_transaction(
+        &self, obj: SharedBytesRef, ret: &mut [u8], config: TransactionConfig
+    ) -> TransactionOutcome {
+        let fp_load = self.ffi_ops().fp_load.unwrap_unchecked();
+        let mut outcome = MaybeUninit::uninit();
+        fp_load(
+            obj.as_mut_ptr() as *mut c_void,
+            ret.as_mut_ptr() as *mut c_void,
+            config.into(),
+            outcome.as_mut_ptr(),
+        );
+        outcome.assume_init().into()
+    }
+
     unsafe fn unchecked_flag_test(&self, flag: SharedFlagRef) -> bool {
         let fp_test = self.ffi_ops().flag_ops.fp_test.unwrap_unchecked();
         fp_test(flag.as_ptr() as *const patomic_transaction_flag_t) != 0
