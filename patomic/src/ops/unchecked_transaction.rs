aa9a7089ef9981f873f729c0debd0289d4d52ee8
doodspav <doodspav@gmail.com>
2026-07-02 22:32:55 +0100

    Add unchecked transaction xchg ops

--- patomic/src/ops/unchecked_transaction.rs
+++ patomic/src/ops/unchecked_transaction.rs
@@ -41,6 +41,57 @@ pub trait UncheckedTransactionOps: FfiOpsTransaction {
         outcome.assume_init().into()
     }
 
+    unsafe fn unchecked_exchange_transaction(
+        &self, obj: SharedBytesRef, desired: &[u8], ret: &mut [u8],
+        config: TransactionConfig
+    ) -> TransactionOutcome {
+        let fp_exchange =
+            self.ffi_ops().xchg_ops.fp_exchange.unwrap_unchecked();
+        let mut outcome = MaybeUninit::uninit();
+        fp_exchange(
+            obj.as_mut_ptr() as *mut c_void,
+            desired.as_ptr() as *const c_void,
+            ret.as_mut_ptr() as *mut c_void,
+            config.into(),
+            outcome.as_mut_ptr(),
+        );
+        outcome.assume_init().into()
+    }
+
+    unsafe fn unchecked_compare_exchange_weak_transaction(
+        &self, obj: SharedBytesRef, expected: &mut [u8], desired: &[u8],
+        config: TransactionConfigWfb
+    ) -> TransactionOutcomeWfb {
+        let fp_cmpxchg_weak =
+            self.ffi_ops().xchg_ops.fp_cmpxchg_weak.unwrap_unchecked();
+        let mut outcome = MaybeUninit::uninit();
+        fp_cmpxchg_weak(
+            obj.as_mut_ptr() as *mut c_void,
+            expected.as_mut_ptr() as *mut c_void,
+            desired.as_ptr() as *const c_void,
+            config.into(),
+            outcome.as_mut_ptr(),
+        );
+        outcome.assume_init().into()
+    }
+
+    unsafe fn unchecked_compare_exchange_strong_transaction(
+        &self, obj: SharedBytesRef, expected: &mut [u8], desired: &[u8],
+        config: TransactionConfigWfb
+    ) -> TransactionOutcomeWfb {
+        let fp_cmpxchg_strong =
+            self.ffi_ops().xchg_ops.fp_cmpxchg_strong.unwrap_unchecked();
+        let mut outcome = MaybeUninit::uninit();
+        fp_cmpxchg_strong(
+            obj.as_mut_ptr() as *mut c_void,
+            expected.as_mut_ptr() as *mut c_void,
+            desired.as_ptr() as *const c_void,
+            config.into(),
+            outcome.as_mut_ptr(),
+        );
+        outcome.assume_init().into()
+    }
+
     unsafe fn unchecked_flag_test(&self, flag: SharedFlagRef) -> bool {
         let fp_test = self.ffi_ops().flag_ops.fp_test.unwrap_unchecked();
         fp_test(flag.as_ptr() as *const patomic_transaction_flag_t) != 0
