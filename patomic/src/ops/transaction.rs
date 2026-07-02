e2606f45a987887e11eccee749e8fd577e5a31ad
doodspav <doodspav@gmail.com>
2026-07-02 03:51:24 +0100

    Use new error

--- patomic/src/ops/transaction.rs
+++ patomic/src/ops/transaction.rs
@@ -4,29 +4,31 @@
 use crate::SharedFlagRef;
 
 use crate::error::{
-    TransactionError, TransactionResult
+    TransactionUnsupportedOpError, TransactionUnsupportedOpResult
 };
 
 use crate::ops::UncheckedTransactionOps;
 
 pub trait TransactionOps: UncheckedTransactionOps {
-    fn flag_test(flag: SharedFlagRef) -> TransactionResult<bool> {
+    fn flag_test(flag: SharedFlagRef) -> TransactionUnsupportedOpResult<bool> {
         if Self::ffi_ops().flag_ops.fp_test.is_none() {
-            return Err(TransactionError::UnsupportedOperation)
+            return Err(TransactionUnsupportedOpError)
         };
         Ok(unsafe { Self::unchecked_flag_test(flag) })
     }
 
-    fn flag_test_set(flag: SharedFlagRef) -> TransactionResult<bool> {
+    fn flag_test_set(
+        flag: SharedFlagRef
+    ) -> TransactionUnsupportedOpResult<bool> {
         if Self::ffi_ops().flag_ops.fp_test_set.is_none() {
-            return Err(TransactionError::UnsupportedOperation)
+            return Err(TransactionUnsupportedOpError)
         };
         Ok(unsafe { Self::unchecked_flag_test_set(flag) })
     }
 
-    fn flag_clear(flag: SharedFlagRef) -> TransactionResult<()> {
+    fn flag_clear(flag: SharedFlagRef) -> TransactionUnsupportedOpResult<()> {
         if Self::ffi_ops().flag_ops.fp_clear.is_none() {
-            return Err(TransactionError::UnsupportedOperation)
+            return Err(TransactionUnsupportedOpError)
         };
         Ok(unsafe { Self::unchecked_flag_clear(flag) })
     }
