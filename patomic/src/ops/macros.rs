be558a75890412f571e5790a415223b0a5f124d8
doodspav <doodspav@gmail.com>
2026-07-04 14:23:35 +0300

    Add tsx macro checks

--- patomic/src/ops/macros.rs
+++ patomic/src/ops/macros.rs
@@ -14,7 +14,7 @@ macro_rules! do_atomic_checks {
             return Err(
                 $crate::error::AtomicOpError::UnsupportedOperation.into()
             );
-        };
+        }
 
         // check that atomic object is suitably aligned
         if !$self.alignment().is_met_by($obj) {
@@ -64,5 +64,63 @@ macro_rules! do_atomic_checks_bit_test {
     };
 }
 
+macro_rules! do_transaction_checks {
+    (
+        $ops:expr,
+        $fp:ident,
+        $config:ident,
+        $obj:ident
+        $(, $bytes:ident)* $(,)?
+    ) => {
+        // check that operation is supported
+        if $ops.$fp.is_none() {
+            return Err(
+                $crate::error::TransactionOpError::UnsupportedOperation.into()
+            );
+        }
+
+        // check that all objects have the expected width
+        {
+            let width = $config.width;
+            if $obj.len() != width {
+                return Err(
+                    $crate::error::TransactionOpError::InvalidSize.into()
+                );
+            }
+            $(
+                if $bytes.len() != width {
+                    return Err(
+                        $crate::error::TransactionOpError::InvalidSize.into()
+                    );
+                }
+            )*
+        }
+    };
+}
+
+macro_rules! do_transaction_checks_bit_test {
+    (
+        $ops:expr,
+        $fp:ident,
+        $config:ident,
+        $obj:ident
+        $(, $bytes:ident)* ;
+        $offset:ident $(,)?
+    ) => {
+        // do initial checks
+        do_transaction_checks!($ops, $fp, $config, $obj $(, $bytes)*);
+
+        // check that offset does not go out of bounds
+        let bit_width = $config.width * (u8::BITS as usize);
+        if $offset >= bit_width || $offset > ::core::ffi::c_int::MAX as usize {
+            return Err(
+                $crate::error::TransactionBitwiseOpError::InvalidOffset.into()
+            )
+        }
+    }
+}
+
 pub (crate) use do_atomic_checks;
 pub (crate) use do_atomic_checks_bit_test;
+pub (crate) use do_transaction_checks;
+pub (crate) use do_transaction_checks_bit_test;
